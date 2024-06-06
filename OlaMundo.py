a40a8377645c70a0c7bb4d9fd142da5f36691db0
Jonatas-Souz <jonatas.souza.1503@gmail.com>
2024-06-06 14:13:52 -0300

    Primeiro envio - Apresentação

---
+++ OlaMundo.py
@@ -0,0 +1,8 @@
+nome = "Jonatas"
+idade = "22"
+cidade = "Itaquaquecetuba"
+formacao = "Estudando Python e Mecatronica"
+
+print(f"Ola meu nome e {nome}, tenho {idade} anos de idade")
+print(f"Moro em {cidade}, estou estudando {formacao}")
+
