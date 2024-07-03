27f7cfc259731c4faf0bf177fcc57c4ff05a281d
Jonatas de Souza Araujo <jonatas.souza.1503@gmail.com>
2024-07-03 19:20:11 -0300

    Delete Aula 2 directory

--- Aula 2/Atividades/Desafio Extra 2.py
+++
@@ -1,7 +0,0 @@
-# Escreav um programa que digite dois caracteres e imprima-os na tela da seguinte forma: "O usuario digitou {caracter1} e {caracter2}"
-
-caracter1 = str(input("Digite o primeiro caracter:"))
-caracter2 = str(input("Digite o segundo caracter:"))
-
-print(f"O usuario digitou {caracter1} e {caracter2}")
-input()
--- Aula 2/Atividades/Desafio Extra 5.py
+++
@@ -1,3 +0,0 @@
-valor=float(input("Digite o valor da prestacao:  "))
-tempo=float(input("Digite o tempo de prestacao (em dias): "))
-taxa=float(input("Digite a taxa da prestacao:  "))
