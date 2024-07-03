b6a77a547f0e648ec2f916987835f20b009560b1
Jonatas de Souza Araujo <jonatas.souza.1503@gmail.com>
2024-07-03 19:24:28 -0300

    Aulas de Python Senai Nami Jafet

---
+++ Aula 1/tipos.py
@@ -0,0 +1,11 @@
+print('Ola Mundo!')
+print("Jonatas")
+nome_completo = "Jonatas de Souza araujo"
+idade = "22"
+cidade = "Itaquaquecetuba"
+turma = "Turma Python"
+
+print(nome_completo)
+#Comentario
+print("Meu nome e: "+nome_completo+ "\nMinha idade é: "+idade+ "\nMoro em:" +cidade) #\n = Pula linha 
+print("Estou na turma de:" +turma)
